56ffd9579d917cb956741e42020aae0b6e42fe92
Burgess Chang <vhtq18w@gmail.com>
2019-11-08 16:24:59 +0800

    Add new upload route(with multi image) and test instance

--- server/resources/record.py
+++ server/resources/record.py
@@ -1,5 +1,12 @@
+import json
+import os
+
+from config.base import BaseConfig
 from flask_restful import reqparse, Resource
+from server import photos
+from server.models.image import Image
 from server.models.record import Record, RecordDetail
+from werkzeug.datastructures import FileStorage
 
 
 class RecordInfo(Resource):
@@ -45,3 +52,61 @@ class RecordInfo(Resource):
         record.insert()
 
         return {'id': record.id}, 201
+
+
+class RecordInfoWithImage(Resource):
+    def post(self):
+        parser = reqparse.RequestParser()
+        parser.add_argument('title', type=str)
+        parser.add_argument('description', type=str)
+        parser.add_argument('phone', type=str)
+        parser.add_argument('mail', type=str)
+        parser.add_argument('picture', type=FileStorage, location='files', action='append')
+        parser.add_argument('source', type=str)
+        args = parser.parse_args()
+        ip = reqparse.request.remote_addr
+        record_detail = RecordDetail(
+            title=args['title'],
+            description=args['description'],
+            source=args['source'],
+            phone=args['phone'],
+            mail=args['mail']
+        )
+        index = 0
+        picture = reqparse.request.files.getlist("picture")
+        for p in picture:
+            f = photos.save(p)
+            url = photos.url(f)
+            realurl = os.path.join(BaseConfig.DATA_IMAGE_DIR + f)
+            image = Image(
+                filename=f,
+                url=url,
+                realurl=realurl
+            )
+            image.insert()
+            if index == 0:
+                record_detail.image1 = image.id
+            elif index == 1:
+                record_detail.image2 = image.id
+            elif index == 2:
+                record_detail.image3 = image.id
+            elif index == 3:
+                record_detail.image4 = image.id
+            elif index == 4:
+                record_detail.image5 = image.id
+            elif index == 5:
+                record_detail.image6 = image.id
+            elif index == 6:
+                record_detail.image7 = image.id
+            elif index == 7:
+                record_detail.image8 = image.id
+            elif index == 8:
+                record_detail.image9 = image.id
+            index += 1
+        record_detail.insert()
+        record = Record(
+            detail=record_detail.id,
+            ip=ip
+        )
+        record.insert()
+        return { 'id': record.id }, 201
--- server/routes/record.py
+++ server/routes/record.py
@@ -1,9 +1,10 @@
 from flask import Blueprint
 from flask_restful import Api
-from server.resources.record import RecordInfo
+from server.resources.record import RecordInfo, RecordInfoWithImage
 
 
 record_bp = Blueprint('record', __name__, url_prefix='/record')
 record_api = Api(record_bp)
 
 record_api.add_resource(RecordInfo, '/')
+record_api.add_resource(RecordInfoWithImage, '/withimage')
